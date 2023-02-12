cf2924bd07d76ba68ba31c3a2bb09f44f4b085f4
unknown <sanjaybaro2017@gmail.com>
2023-02-12 11:23:25 +0530

    I have added two code for prime number and palindrom word please find the error here and fixed it

---
+++ palindrom.js
@@ -0,0 +1,20 @@
+let str="naman";
+
+let N=str.length;
+
+let bag="";
+
+for(let i=N-1;i>=0;i--){
+
+bag=bag+str[i];
+
+}
+if(bag==str){
+
+print("Yes);
+
+}else{
+
+print("No);
+
+}
---
+++ prime.js
@@ -0,0 +1,24 @@
+
+let number=7;
+
+let count=0;
+
+for(let i=1;i<=number;i++)
+{
+
+if(number%i==0)
+{
+count++;
+
+}
+
+}
+if(count==2){
+
+print("Prime Number)
+
+}else{
+
+print("nOT a prime number);
+
+}
